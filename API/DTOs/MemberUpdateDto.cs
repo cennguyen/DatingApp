8c86ed0bafacca4ca837184f90c2733a5f49fdcf
cennguyen <annp62923@gmail.com>
2021-01-29 15:58:38 +0700

    Update edit photo function

--- API/DTOs/MemberUpdateDto.cs
+++ API/DTOs/MemberUpdateDto.cs
@@ -2,12 +2,13 @@ namespace API.DTOs
 {
     public class MemberUpdateDto
     {
-        public int Age { get; set; }
+
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public int Age { get; set; }
 
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,10 +23,7 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
-        
+
+
     }
 }
